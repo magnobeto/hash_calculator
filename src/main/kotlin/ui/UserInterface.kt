a95ec817ee28f33000c58a7544a5eecdcd06c2ea
roberto.campos <roberto.magno@g.globo>
2023-11-20 00:04:54 -0300

    refactor

--- src/main/kotlin/ui/UserInterface.kt
+++ src/main/kotlin/ui/UserInterface.kt
@@ -2,23 +2,32 @@ package ui
 
 import calculator.HashCalculator
 import di.injectHashCalculator
+import utils.MD5
+import utils.SHA_1
+import utils.SHA_256
 
 class UserInterface(private val calculator: HashCalculator = injectHashCalculator()) {
 
     private lateinit var userInput: String
 
     fun presentation() {
-        // Solicita ao usuário que digite um valor
         println("-----Calculadora Hash------")
         println("Digite o algoritmo que deseja utilizar:")
         println("1 - MD5")
         println("2 - SHA1")
         println("3 - SHA256")
 
-        // Lê o valor digitado pelo usuário
         userInput = readlnOrNull() ?: ""
 
-        // Exibe o valor digitado
         println("Você digitou: $userInput")
+        selectAlgorithm()
+    }
+
+    private fun selectAlgorithm() {
+        when {
+            userInput.contains(MD5, true) -> calculator.getResult(userInput)
+            userInput.contains(SHA_1, true) -> calculator.getResult(userInput)
+            userInput.contains(SHA_256, true) -> calculator.getResult(userInput)
+        }
     }
 }

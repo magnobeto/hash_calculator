32a9e8f5905a4495bde81fb1b77ab9b72e66b791
roberto.campos <roberto.magno@g.globo>
2023-11-29 14:51:18 -0300

    refactor user interface

--- src/main/kotlin/ui/UserInterface.kt
+++ src/main/kotlin/ui/UserInterface.kt
@@ -12,6 +12,7 @@ class UserInterface(private val calculator: HashCalculator = injectHashCalculato
     }
 
     private fun presentation() {
+        println()
         println("Digite o algoritmo que deseja utilizar:")
         println("1 - MD5")
         println("2 - SHA1")
@@ -27,22 +28,43 @@ class UserInterface(private val calculator: HashCalculator = injectHashCalculato
 
     private fun selectAlgorithm(choosedAlgorithm: String, inputText: String) {
         when {
-            regexMD5.matches(choosedAlgorithm)-> {
+            regexMD5.matches(choosedAlgorithm) -> {
                 println("$ALGORITHM_SELECTED $MD5")
                 calculator.algorithmMd5(inputText)
+                decideContinueOrEndProgram()
             }
+
             regexSHA1.matches(choosedAlgorithm) -> {
                 println("$ALGORITHM_SELECTED $SHA_1")
                 calculator.algorithmSha1(inputText)
+                decideContinueOrEndProgram()
             }
+
             regexSHA256.matches(choosedAlgorithm) -> {
                 println("$ALGORITHM_SELECTED $SHA_256")
                 calculator.algorithmSha256(inputText)
+                decideContinueOrEndProgram()
             }
+
             else -> {
                 println("Não escolheu um algoritmo válido, tente novamente.")
                 presentation()
             }
+        }
+    }
+
+    private fun decideContinueOrEndProgram() {
+        println()
+        println("Digite encerrar?")
+        println("1 - Sim")
+        println("2 - Não")
+        val inputText = readlnOrNull() ?: ""
+        when {
+            regexContinueProgram.matches(inputText) -> {
+                presentation()
+            }
+
+            regexEndProgram.matches(inputText) -> {}
         }
     }
 }
--- src/main/kotlin/utils/RegexUtils.kt
+++ src/main/kotlin/utils/RegexUtils.kt
@@ -3,3 +3,5 @@ package utils
 val regexMD5 = Regex("(MD5|1)", RegexOption.IGNORE_CASE)
 val regexSHA1 = Regex("(SHA-1|SHA1|2)", RegexOption.IGNORE_CASE)
 val regexSHA256 = Regex("(SHA-256|SHA256|3)", RegexOption.IGNORE_CASE)
+val regexEndProgram = Regex("(SIM|1)", RegexOption.IGNORE_CASE)
+val regexContinueProgram = Regex("(NAO|NÃO|2)", RegexOption.IGNORE_CASE)
